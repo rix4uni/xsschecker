bea5039f28e3f6e9a456cf4b0b8b59ed91cd1146
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2022-07-22 14:55:53 +0530

    Create xsschecker.go

---
+++ xsschecker.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"sync"
+	"bufio"
+	"net/http"
+	"fmt"
+	"os"
+	"strings"
+	"io/ioutil"
+)
+
+func main(){
+	// fmt.Println("\n")
+	fmt.Println("frequester tool By tojojo !!")
+	fmt.Println("\\__(-_-)__/")
+	// fmt.Println("\n")
+
+	colorReset := "\033[0m"
+	colorRed := "\033[31m"
+  // colorGreen := "\033[32m"
+
+
+	sc := bufio.NewScanner(os.Stdin)
+
+	jobs := make(chan string)
+	var wg sync.WaitGroup
+
+	for i:= 0; i < 20; i++{
+
+		wg.Add(1)
+		go func(){
+			defer wg.Done()
+			for domain := range jobs {
+
+				resp, err := http.Get(domain)
+				if err != nil{
+					continue
+				}
+				body, err := ioutil.ReadAll(resp.Body)
+				if err != nil {
+	      			fmt.Println(err)
+	   			}
+	   			sb := string(body)
+	   			check_result1 := strings.Contains(sb , "prompt(1)")
+	   			check_result2 := strings.Contains(sb , "confirm(1)")
+	   			check_result3 := strings.Contains(sb , "alert(1)")
+	   			// fmt.Println(check_result)
+	   			if check_result1 != false {
+	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
+	   			}else if check_result2 != false {
+	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
+	   			}else if check_result3 != false {
+	   				fmt.Println(string(colorRed),"Vulnerable To XSS:", domain,string(colorReset))
+	   			}
+			}
+			
+   		}()
+
+	}
+
+
+
+	for sc.Scan(){
+		domain := sc.Text()
+		jobs <- domain		
+		
+
+	}
+	close(jobs)
+	wg.Wait()
+}
