76a904642c10bfc331eb83784616909ff5733c49
Bhagirath Saxena <72344025+rix4uni@users.noreply.github.com>
2023-08-14 17:22:03 +0530

    Update xsschecker.go

--- xsschecker.go
+++ xsschecker.go
@@ -1,59 +1,64 @@
 package main
 
 import (
-	"sync"
 	"bufio"
-	"net/http"
+	"flag"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"strings"
-	"io/ioutil"
+	"sync"
 )
 
-func main(){
+func main() {
+
+	// Define the flags
+	matchString := flag.String("match", "", "String to match against the domain response")
+	onlyVulnerable := flag.Bool("vuln", false, "Print only vulnerable URLs if set")
+	flag.Parse()
+
+	if *matchString == "" {
+		fmt.Println("Please provide a match string using the -match flag.")
+		return
+	}
 
 	sc := bufio.NewScanner(os.Stdin)
 
 	jobs := make(chan string)
 	var wg sync.WaitGroup
 
-	for i:= 0; i < 20; i++{
+	for i := 0; i < 20; i++ {
 
 		wg.Add(1)
-		go func(){
+		go func() {
 			defer wg.Done()
 			for domain := range jobs {
 
 				resp, err := http.Get(domain)
-				if err != nil{
+				if err != nil {
 					continue
 				}
 				body, err := ioutil.ReadAll(resp.Body)
 				if err != nil {
-	      			fmt.Println(err)
-	   			}
-	   			sb := string(body)
-	   			check_result1 := strings.Contains(sb , "prompt(1)")
-	   			check_result2 := strings.Contains(sb , "confirm(1)")
-	   			check_result3 := strings.Contains(sb , "alert(1)")
-	   			
-	   			if check_result1 != false {
-	   				fmt.Println("\033[1;31mVulnerable: "+domain+"\033[0;0m")
-	   			}else if check_result2 != false {
-	   				fmt.Println("\033[1;31mVulnerable: "+domain+"\033[0;0m")
-	   			}else if check_result3 != false {
-	   				fmt.Println("\033[1;31mVulnerable: "+domain+"\033[0;0m")
-				}else {
-					fmt.Println("\033[1;30mNot Vulnerable: "+domain+"\033[0;0m")
+					fmt.Println(err)
+				}
+				sb := string(body)
+
+				isVulnerable := strings.Contains(sb, *matchString)
+
+				if isVulnerable {
+					fmt.Println("\033[1;31mVulnerable: " + domain + "\033[0;0m")
+				} else if !*onlyVulnerable { // If onlyVulnerable is false, print non-vulnerable URLs
+					fmt.Println("\033[1;35mNot Vulnerable: " + domain + "\033[0;0m")
 				}
 			}
-   		}()
+		}()
 	}
 
-	for sc.Scan(){
+	for sc.Scan() {
 		domain := sc.Text()
-		jobs <- domain		
-	
+		jobs <- domain
 	}
 	close(jobs)
 	wg.Wait()
